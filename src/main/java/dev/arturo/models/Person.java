ff2a343d96fd22068d19317983e4f91976a7907f
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-07-10 20:04:18 +0200

    making the Class Person, getters, setters, constructor and method

--- src/main/java/dev/arturo/models/Person.java
+++ src/main/java/dev/arturo/models/Person.java
@@ -1,5 +1,69 @@
 package dev.arturo.models;
 
 public class Person {
+    String name;
+    String lastName;
+    Integer borningYear;
+    String dni;
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public Integer getBorningYear() {
+        return borningYear;
+    }
+
+
+    public void setBorningYear(Integer borningYear) {
+        this.borningYear = borningYear;
+    }
+
+
+    public String getDni() {
+        return dni;
+    }
+
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    
+    public Person(String name, String lastName, Integer borningYear, String dni) {
+        this.name = name;
+        this.lastName = lastName;
+        this.borningYear = borningYear;
+        this.dni = dni;
+    }
+
+
+    public String printingPerson(){
+        
+        System.out.println(name);
+        System.out.println(lastName);
+        System.out.println(borningYear);
+        System.out.println(dni);
+
+        return printingPerson() ;
+    }
+    
     
 }
